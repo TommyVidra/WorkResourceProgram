f0813b02fbd4cf8beca7f474439ea0f61a50f22b
VidraTo <tomislav.vidovic@fer.hr>
2020-04-14 16:31:47 +0200

    First commit

---
+++ StariApp/StariApp/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace StariApp
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SideMenuPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.EventPanel = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SideMenuPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SideMenuPanel
+            // 
+            this.SideMenuPanel.Controls.Add(this.button9);
+            this.SideMenuPanel.Controls.Add(this.button8);
+            this.SideMenuPanel.Controls.Add(this.button7);
+            this.SideMenuPanel.Controls.Add(this.button6);
+            this.SideMenuPanel.Controls.Add(this.button5);
+            this.SideMenuPanel.Controls.Add(this.button3);
+            this.SideMenuPanel.Controls.Add(this.button4);
+            this.SideMenuPanel.Controls.Add(this.button2);
+            this.SideMenuPanel.Controls.Add(this.button1);
+            this.SideMenuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.SideMenuPanel.Location = new System.Drawing.Point(0, 0);
+            this.SideMenuPanel.Name = "SideMenuPanel";
+            this.SideMenuPanel.Size = new System.Drawing.Size(183, 549);
+            this.SideMenuPanel.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(183, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Zaposlenici";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(0, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(183, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Resursi";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.Location = new System.Drawing.Point(0, 150);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(183, 50);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Radovi";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.Location = new System.Drawing.Point(0, 100);
+            this.button4.Name = "button4";
+            this.button4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button4.Size = new System.Drawing.Size(183, 50);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Napomene";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.Location = new System.Drawing.Point(0, 200);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(183, 50);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Događaji";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // EventPanel
+            // 
+            this.EventPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.EventPanel.Location = new System.Drawing.Point(183, 148);
+            this.EventPanel.Name = "EventPanel";
+            this.EventPanel.Size = new System.Drawing.Size(703, 401);
+            this.EventPanel.TabIndex = 1;
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button6.Location = new System.Drawing.Point(0, 499);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(183, 50);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Izbriši";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button7.Location = new System.Drawing.Point(0, 449);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(183, 50);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Izmjeni";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button8.Location = new System.Drawing.Point(0, 399);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(183, 50);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Dodaj";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(183, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(703, 150);
+            this.panel1.TabIndex = 2;
+            // 
+            // button9
+            // 
+            this.button9.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button9.Location = new System.Drawing.Point(0, 349);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(183, 50);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Pregled";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 549);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.EventPanel);
+            this.Controls.Add(this.SideMenuPanel);
+            this.Name = "form1";
+            this.Text = "Aplikacija_Rad";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SideMenuPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel SideMenuPanel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel EventPanel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ StariApp/StariApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StariApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\Projects\\StariApp\\StariApp\\StariApp\\StariAppDB.mdf;Integrated Security=True");
+            SqlCommand command = new SqlCommand();
+            SqlDataReader reader = null;
+            try
+            {
+                // 2. Open the connection
+                connection.Open();
+
+                // 3. Pass the connection to a command object
+                SqlCommand cmd = new SqlCommand("select * from Resource", connection);
+
+                //
+                // 4. Use the connection
+                //
+
+                // get query results
+                reader = cmd.ExecuteReader();
+
+                // print the CustomerID of each record
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[1]);
+                }
+            }
+            finally
+            {
+                // close the reader
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+
+                // 5. Close the connection
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new form1());
+        }
+    }
+}
